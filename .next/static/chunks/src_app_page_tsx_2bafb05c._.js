acfb84623496c66399e1e0e6f925eaf5258a492f
Mthbarroncas <barroncas1998@gmail.com>
2025-07-18 21:25:51 -0300

    Add .next/static/chunks/src_app_page_tsx_2bafb05c._.js 1752884760311

---
+++ .next/static/chunks/src_app_page_tsx_2bafb05c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_24211c3a._.js",
+  "static/chunks/src_30b5f5ea._.js"
+],
+    source: "dynamic"
+});
